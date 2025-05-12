499778117cdf1b98c3375626ae7e613a68132e1d
dileepc-rafay <dileep.c@rafay.co>
2025-05-12 14:00:01 +0530

    eks template changes

---
+++ res-template/eks-template/main.tf
@@ -0,0 +1,109 @@
+resource "rafay_eks_cluster" "eks-cluster-1" {
+  cluster {
+    kind = "Cluster"
+    metadata {
+      name    = var.cluster_name
+      project = var.project_name
+    }
+    spec {
+      type           = "eks"
+      blueprint      = "default"
+      blueprint_version = "1.13.0"
+      cloud_provider = "dileep-aws"
+      cni_provider   = "aws-cni"
+      proxy_config   = {}
+    }
+  }
+  cluster_config {
+    apiversion = "rafay.io/v1alpha5"
+    kind       = "ClusterConfig"
+    metadata {
+      name    = "eks-cluster-dileep-may-12"
+      region  = var.region
+      version = "1.21"
+    }
+    iam {
+      with_oidc = true
+      service_accounts {
+        metadata {
+          name = "test-irsa"
+          namespace = "default"
+        }
+        attach_policy = <<EOF
+        {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": "ec2:Describe*",
+          "Resource": "*"
+        },
+        {
+          "Effect": "Allow",
+          "Action": "ec2:AttachVolume",
+          "Resource": "*"
+        },
+        {  
+          "Effect": "Allow",
+          "Action": "ec2:DetachVolume",
+          "Resource": "*"
+        },
+        {
+          "Effect": "Allow",
+          "Action": ["ec2:*"],
+          "Resource": ["*"]
+            },
+        {
+          "Effect": "Allow",
+          "Action": ["elasticloadbalancing:*"],
+          "Resource": ["*"]
+         }
+       ]
+    }
+    EOF
+      }
+    }
+    vpc {
+      cidr = "192.168.0.0/16"
+      cluster_endpoints {
+        private_access = true
+        public_access  = false
+      }
+      nat {
+        gateway = "Single"
+      }
+    }
+    node_groups {
+      name       = "testing-dileep"
+      ami_family = "AmazonLinux2"
+      iam {
+        iam_node_group_with_addon_policies {
+          image_builder = true
+          auto_scaler   = true
+        }
+      }
+      instance_type    = "t3.large"
+      desired_capacity = 1
+      min_size         = 1
+      max_size         = 2
+      max_pods_per_node = 50
+      version          = "1.21"
+      volume_size      = 100
+      volume_type      = "gp3"
+      private_networking = true
+    }
+    addons {
+      name = "vpc-cni"
+      version = "latest"
+    }
+    addons {
+      name = "kube-proxy"
+      version = "latest"
+
+    }
+    addons {
+      name = "coredns"
+      version = "latest"
+    }
+  }
+}
---
+++ res-template/eks-template/variables.tf
@@ -0,0 +1,16 @@
+variable cluster_name {
+  type        = string
+  default     = "eks-cluster-dileep-may-12"
+  description = "Cluster name "
+}
+
+variable project_name {
+  type        = string
+  default     = "testing-dileep"
+  description = "Project name"
+}
+variable region {
+  type        = string
+  default     = "us-west-2"
+  description = "AWS region"
+}
